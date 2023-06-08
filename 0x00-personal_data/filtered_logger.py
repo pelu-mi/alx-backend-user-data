78a58b6f80a2947b8809e9594b35a4e935bdaf09
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-08 13:52:22 +0100

    Type annotation fix for task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Return the log message obfuscated
     """
