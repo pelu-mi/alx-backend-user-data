36353a77b7d48a20a531428a294f9c6c974636bc
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-08 15:53:14 +0100

    Added changes for task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     """ Return a hashed password
     """
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if hashed password is a valid encryption of password
+    """
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
