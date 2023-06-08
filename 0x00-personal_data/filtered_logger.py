e2c0182377fd8da107d7f3e026195df4886d855b
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-08 14:11:47 +0100

    Added file for task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,9 @@
 """ Module to implement methods handling personal data
 """
 
-from typing import List
 import re
+import logging
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -14,3 +15,23 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub("{}=.*?{}".format(f, separator),
                          "{}={}{}".format(f, redaction, separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format string based on format and filter_datum
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
