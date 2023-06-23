d373d2131216a8ed3e4db75fa6e3023072916666
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 07:21:40 -0700

    Added files for task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module for API Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """ Authentication system template
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determine if authentication is required
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Get the current user
+        """
+        return None
