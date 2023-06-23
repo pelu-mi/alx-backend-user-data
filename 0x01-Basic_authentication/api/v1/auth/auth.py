58e7e981ca4ebc3c55e6a8f798ceb2d8b0ae5ef8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 07:30:32 -0700

    Added changes for task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determine if authentication is required
         """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
