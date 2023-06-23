f43fdb7bcc6c4cde77770808d78180aac301d2ba
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 08:04:33 -0700

    Added changes for task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Authentication system
     """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extract base64 encoded string from the header
+        """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split()[1]
