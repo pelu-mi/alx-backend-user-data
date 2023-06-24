b33121c2a0b7a033a5ae71658fa54e24e3f7b53d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 23:26:03 -0700

    Added changes for task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,6 +15,13 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        sess_id = str(uuid.uuid4())
-        self.user_id_by_session_id[sess_id] = user_id
-        return sess_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Get the user_id based on the session_id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
