434d3b3121f767db116912d4dd89c031d068737e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 23:47:52 -0700

    Added changes for task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module for Session Auth implementation
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -25,3 +26,12 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Get the current user
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
