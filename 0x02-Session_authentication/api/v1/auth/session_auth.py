9c3367b01c2e12a718a80f301f4130dea0390258
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 23:20:12 -0700

    Added changes for task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,19 @@
 """ Module for Session Auth implementation
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Auth implementation
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session id for a user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        sess_id = str(uuid.uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
