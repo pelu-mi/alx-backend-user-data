aa08d60738235c9cc51eb8bb102fa141b4b322dd
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-23 23:11:25 -0700

    Added changes for task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module for Session Auth implementation
+"""
+from api.v1.auth.auth import Auth
+
+
+
+class SessionAuth(Auth):
+    """ Session Auth implementation
+    """
