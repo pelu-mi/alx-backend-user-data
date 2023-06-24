e659d3280b57cb2fcaa17a54dc7b3140beb65e8c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-24 00:42:52 -0700

    Fix 2 for task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -24,7 +24,7 @@ def login():
         return jsonify({"error": "password missing"}), 400
 
     users = User.search({'email': email})
-    if users is None:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
     user = users[0]
