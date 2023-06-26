7ee0cdb1e3ce45fd0e55df2fd8d37f56b0be547f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-25 23:04:57 -0700

    Added changes for task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -41,4 +43,21 @@ class DB:
         except Exception:
             self._session.rollback()
             user = None
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find the first user with matching kwargs arguments
+        """
+        # Check the validity of kwargs
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+        # Run query
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
