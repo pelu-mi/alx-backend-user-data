dcfafabd7443d4a7318c55e23bb8eb32b349134d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-27 06:34:55 -0700

    Added changes for task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module for authentication implementation
 """
+import uuid
 import bcrypt
 from user import User
 from db import DB
@@ -12,6 +13,12 @@ def _hash_password(password: str) -> bytes:
     """ Create salted hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Generate uuid
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
