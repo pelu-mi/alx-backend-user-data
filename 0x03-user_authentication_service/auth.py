d7e126fa1c13d67cc384dcf05db013a893b01efa
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-26 20:47:03 -0700

    Added changes for task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,29 @@
 """ Module for authentication implementation
 """
 import bcrypt
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ Create salted hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register User
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+        raise ValueError('User {} already exists'.format(email))
