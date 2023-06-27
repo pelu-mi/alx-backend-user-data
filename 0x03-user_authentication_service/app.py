ce4d93e38130aa8cfedeb776033eedd1b1d199bf
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-27 07:30:27 -0700

    Fix 1 for task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,6 +60,7 @@ def profile() -> str:
     """ GET /profile
     """
     session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
     return jsonify({"email": user.email}), 200
